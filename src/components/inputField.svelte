7c0b28e311dd83cde3d46e5cf696563c3eef99a3
Antoni Siekański <catus.pl@gmail.com>
2023-04-04 14:57:22 +0200

    Html layout

---
+++ src/components/inputField.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let label: string = Math.random().toString();
+	export let placeholder: string = label;
+</script>
+
+<div class="field">
+	<label for={label}>{label}</label>
+	<input type="number" name={label} id={label} {placeholder} />
+	<p class="error">Error</p>
+</div>
+
+<style>
+	.error {
+		visibility: hidden;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-Day DD Month MM Year YYYY -- years -- months -- days
+<script lang="ts">
+	import InputField from '../components/inputField.svelte';
+</script>
+
+<main>
+	<form action="">
+		<InputField label="Day" placeholder="DD" />
+		<InputField label="Month" placeholder="MM" />
+		<InputField label="Year" placeholder="YYYY" />
+
+		<button type="submit" class="btn-submit" />
+	</form>
+
+	<h2><span class="value">--</span>years</h2>
+	<h2><span class="value">--</span>months</h2>
+	<h2><span class="value">--</span>days</h2>
+</main>
+
+<style>
+</style>

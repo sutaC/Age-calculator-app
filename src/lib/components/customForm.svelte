e21da4d25c31f0dabf3a6985949512b63a58e318
Antoni Siekański <catus.pl@gmail.com>
2023-04-04 16:28:33 +0200

    Mobile view styled

---
+++ src/lib/components/customForm.svelte
@@ -0,0 +1,46 @@
+<form action="">
+	<slot />
+
+	<button type="submit" />
+</form>
+
+<style>
+	form {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+
+		box-sizing: border-box;
+		margin-bottom: 1rem;
+		padding: 0.5rem;
+
+		position: relative;
+
+		border-bottom: 1px solid var(--clr-Light-grey);
+	}
+
+	button {
+		position: absolute;
+		bottom: 0;
+
+		transform: translate(0, 50%);
+
+		border: none;
+		border-radius: 100%;
+
+		--btn-size: 2rem;
+
+		width: var(--btn-size);
+		height: var(--btn-size);
+
+		background-color: var(--clr-Purple);
+		background-image: url('$lib/images/icon-arrow.svg');
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: 1rem;
+	}
+	button:active {
+		background-color: var(--clr-Off-black);
+	}
+</style>
